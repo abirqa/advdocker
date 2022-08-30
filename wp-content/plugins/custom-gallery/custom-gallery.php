14d2968c02723d62cf7b2f4ed6e32fff80a1fb6b
abirqa <abir@theqa.in>
2022-08-30 17:18:14 +0530

    Files Added

---
+++ wp-content/plugins/custom-gallery/custom-gallery.php
@@ -0,0 +1,30 @@
+
+<?php 
+/** 
+ * Custom Gallery Plugin
+ * 
+ * @package Custom Gallery Plugin 
+ * @author Abir
+ * @copyright 2020 Your Name 
+ * @license GPL-2.0-or-later 
+ * 
+ * @wordpress-plugin 
+ * Plugin Name: Custom Gallery Plugin
+ * Plugin URI: https://mysite.com/hello-world 
+ * Description: Prints "Custom Gallery Plugin" in WordPress admin. 
+ * Version: 0.0.1 
+ * Author: Abir
+ * Author URI: https://mysite.com 
+ * Text Domain: Custom Gallery Plugin 
+ * License: GPL v2 or later 
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt */
+
+?>
+
+
+
+
+
+
+
+
---
+++ wp-content/themes/test-theme/home-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Template Name: Full Width Page
+*
+* @package WordPress
+* @subpackage Twenty_Fourteen
+* @since Twenty Fourteen 1.0
+*/
+
+get_header(); 
+?>
+
+
+<h1>hellow orld</h1>
+
+
+
+<?php 
+get_footer(); 
+?>
